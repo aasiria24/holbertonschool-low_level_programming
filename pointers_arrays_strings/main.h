59cb5b247bb01c2dba31ab41c19b97e6b7679066
aasiria24 <amaalmoasiri@gmail.com>
2025-10-20 13:48:43 +0000

    ?

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);  // ← هذا السطر ضروري
+int _isupper(int c);
+int _isdigit(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
