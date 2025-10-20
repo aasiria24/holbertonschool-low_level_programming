7807c359946c9108f2977074eb8b91b2bbee7f68
aasiria24 <amaalmoasiri@gmail.com>
2025-10-20 13:53:18 +0000

    z to a?

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to the string to be printed in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print the string in reverse */
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
