5c9d51b78aa2424fc4989334a2c4a1baffef6832
aasiria24 <amaalmoasiri@gmail.com>
2025-10-21 07:19:11 +0000

    compare?

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 
+ * - 0 if the strings are equal
+ * - negative value if s1 is less than s2
+ * - positive value if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
