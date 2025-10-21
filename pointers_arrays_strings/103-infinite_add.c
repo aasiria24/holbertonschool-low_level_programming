f3807a4b698ebaba4c825c97a25934a7e826355d
aasiria24 <amaalmoasiri@gmail.com>
2025-10-21 07:57:21 +0000

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers represented as strings
+ * @n1: First number string
+ * @n2: Second number string
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
+ *
+ * Return: Pointer to result or 0 if result can't be stored in r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, carry = 0;
+	int i, j, k, sum;
+
+	/* Calculate lengths of n1 and n2 */
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+
+	/* Check if result can fit in buffer */
+	if (len1 > size_r - 2 || len2 > size_r - 2)
+		return (0);
+
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
+	carry = 0;
+
+	/* Add digits from right to left */
+	while (i >= 0 || j >= 0 || carry)
+	{
+		if (k >= size_r - 1)
+			return (0);
+
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+
+		carry = sum / 10;
+		r[k++] = (sum % 10) + '0';
+	}
+
+	/* Add null terminator */
+	r[k] = '\0';
+
+	/* Reverse the result string */
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+	}
+
+	return (r);
+}
