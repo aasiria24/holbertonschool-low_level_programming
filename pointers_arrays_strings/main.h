180d3ad2ba45bc6be244d05198e0dddf0b146c16
aasiria24 <amaalmoasiri@gmail.com>
2025-10-27 08:16:00 +0000

    where's M?

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -38,4 +38,5 @@ void reverse_string(char *str, int len);
 void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
