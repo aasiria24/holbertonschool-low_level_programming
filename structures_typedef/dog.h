89484c6b05898230876ead8c6fa022b14436bddc
aasiria24 <amaalmoasiri@gmail.com>
2025-11-04 14:25:28 +0000

    who let the dogs out?

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- **/
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
