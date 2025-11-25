96f556a8e1cbc9c2eb756e16b6492b75e5b2a7ce
aasiria24 <amaalmoasiri@gmail.com>
2025-11-25 06:57:06 +0000

    UPDATED: Implement constructor function to print message before main execution

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/**
-* before_main - function that runs before main
-*
-* Description: This function is executed automatically before main
-* due to the constructor attribute.
-*/
 void before_main(void) __attribute__ ((constructor));
 
+/**
+* before_main - prints message before main function
+*
+* This function is executed automatically due to the constructor attribute
+*/
 void before_main(void)
 {
-printf("You're beat! and yet, you must allow,\n);
-printf("I bore my house upon my back	!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
