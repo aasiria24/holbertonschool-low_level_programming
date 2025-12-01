751d2cee4973667892f21ede9b4b0c72945a6008
aasiria24 <amaalmoasiri@gmail.com>
2025-12-01 07:23:51 +0000

    fixing the errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,31 +23,66 @@ char buf[BUFFER_SIZE];
 mode_t perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 if (ac != 3)
-dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+{
+printf("Usage: cp file_from file_to\n");
+printf("97\n");
+exit(97);
+}
 
 fd_from = open(av[1], O_RDONLY);
 if (fd_from == -1)
-dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+{
+printf("Error: Can't read from file %s\n", av[1]);
+printf("98\n");
+exit(98);
+}
 
 fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 if (fd_to == -1)
-dprintf(2, "Error: Can't write to %s\n", av[2]), close(fd_from), exit(99);
+{
+printf("Error: Can't write to %s\n", av[2]);
+printf("99\n");
+close(fd_from);
+exit(99);
+}
 
 while ((r = read(fd_from, buf, BUFFER_SIZE)) > 0)
 {
 w = write(fd_to, buf, r);
 if (w != r)
-dprintf(2, "Error: Can't write to %s\n", av[2]), close(fd_from), close(fd_to), exit(99);
+{
+printf("Error: Can't write to %s\n", av[2]);
+printf("99\n");
+close(fd_from);
+close(fd_to);
+exit(99);
+}
 }
 
 if (r == -1)
-dprintf(2, "Error: Can't read from file %s\n", av[1]), close(fd_from), close(fd_to), exit(98);
+{
+printf("Error: Can't read from file %s\n", av[1]);
+printf("98\n");
+close(fd_from);
+close(fd_to);
+exit(98);
+}
 
 if (close(fd_from) == -1)
-dprintf(2, "Error: Can't close fd %d\n", fd_from), close(fd_to), exit(100);
+{
+printf("Error: Can't close fd %d\n", fd_from);
+printf("100\n");
+close(fd_to);
+exit(100);
+}
 
 if (close(fd_to) == -1)
-dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+{
+printf("Error: Can't close fd %d\n", fd_to);
+printf("100\n");
+exit(100);
+}
 
 return (0);
 }
+
