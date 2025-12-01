3f9316bd56ab7773db2b1879794dfa643ebac5da
aasiria24 <amaalmoasiri@gmail.com>
2025-12-01 07:59:02 +0000

    FINAL: fixing betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
 * main - Copies content of a file to another file
@@ -17,37 +17,38 @@
 */
 int main(int ac, char *av[])
 {
-int fd_from, fd_to;
-ssize_t r, w;
-char buf[BUFFER_SIZE];
-mode_t perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+int f1, f2;
+ssize_t n;
+char buf[BUF_SIZE];
 
 if (ac != 3)
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
-fd_from = open(av[1], O_RDONLY);
-if (fd_from == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+f1 = open(av[1], O_RDONLY);
+if (f1 == -1)
+dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
-if (fd_to == -1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), close(fd_from), exit(99);
+f2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (f2 == -1)
+dprintf(2, "Error: Can't write to %s\n", av[2]), close(f1), exit(99);
 
-while ((r = read(fd_from, buf, BUFFER_SIZE)) > 0)
+while ((n = read(f1, buf, BUF_SIZE)) > 0)
+if (write(f2, buf, n) != n)
 {
-w = write(fd_to, buf, r);
-if (w != r)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), close(fd_from), close(fd_to), exit(99);
+dprintf(2, "Error: Can't write to %s\n", av[2]);
+close(f1), close(f2), exit(99);
 }
 
-if (r == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), close(fd_from), close(fd_to), exit(98);
+if (n == -1)
+dprintf(2, "Error: Can't read from file %s\n", av[1]),
+close(f1), close(f2), exit(98);
 
-if (close(fd_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), close(fd_to), exit(100);
+if (close(f1) == -1)
+dprintf(2, "Error: Can't close fd %d\n", f1),
+close(f2), exit(100);
 
-if (close(fd_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+if (close(f2) == -1)
+dprintf(2, "Error: Can't close fd %d\n", f2), exit(100);
 
 return (0);
 }
